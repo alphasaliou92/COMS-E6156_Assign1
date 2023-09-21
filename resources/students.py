e0aa8eeb30eb5cbe2f15450f2949e88f1e2a7fcd
alphasaliou92 <38296852+alphasaliou92@users.noreply.github.com>
2023-09-21 10:20:26 -0400

    alpha

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/donaldferguson/Dropbox/000-Examples/e6156-microservice-1/resources/old-students.json"
+        "/Users/alphasalioubah/Documents/Courses/Fall 2023/Cloud Computing/Projects/e6156-microservice-1/resources/old-students.json"
 
     def __init__(self):
         self.students = None
